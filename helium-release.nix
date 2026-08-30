d276f1ffd86583f0454a3adea53e6204d143187b
clamlum <172550814+clamlum@users.noreply.github.com>
2026-08-30 04:37:35 +0000

    Update Helium

--- helium-release.nix
+++ helium-release.nix
@@ -1,7 +1,7 @@
 {
-  version = "0.16.1.1";
+  version = "0.16.2.1";
 
-  linux_x86_64_hash = "sha256-u1L7/oXmST24AWEtrSiJDUwd7bm7fqp1gDj17kLWZuE=";
-  linux_aarch64_hash = "sha256-3nV7OqGUwONcRlwbBaTvQEaM6UlPtN3CHtyG9zpbSIQ=";
-  darwin_aarch64_hash = "sha256-tSNdtdtJ1HhMhQabuftzG6OWxaFVBoShKKHy0yeNHgE=";
+  linux_x86_64_hash = "sha256-gAg4BpJyhwpvT8nq3wF8CBn32Jq/YHEXCAsHnUv3wBc=";
+  linux_aarch64_hash = "sha256-7l33oPwHvlGrokwNq57T57eUcZkTevjZ0mqfWPfTjfc=";
+  darwin_aarch64_hash = "sha256-aoi566LGNGht1XhyyZCla7kl7lk0WzTq33aBMkKhyu8=";
 }
